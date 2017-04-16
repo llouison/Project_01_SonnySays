5869ced67dd5eabcdd52b910af9cd84a7305ee93
lisa louison <lisa.louison@gmail.com>
2017-04-16 12:09:21 -0400

    consolidated app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-console.log('connected');
+console.log('Let\'s Play Sonny Says!');
 
-// function startGame(){
     //creating variables for the game
     var startButton = document.querySelector('#start');
     var restartButton = document.querySelector('#restart');
     restartButton.style.display = 'none';
-    var round = 1;
     var roundDisplay = document.querySelector('#display');
+    var round = 1;
     var time = 1000;
     var computerSequence = [];
-    // var playerSequence = [];
     
     //locating the bunny body parts
     var a = document.querySelector('#left_ear');
@@ -20,18 +18,12 @@ console.log('connected');
     var f = document.querySelector('#right_leg');
     //creating an array of the possible moves
     var possibleMoves = [a, b, c, d, e, f];
-    //initiating the game
-    // playRound();
-
-
 
 
     function playRound(){
         // disabling the start button
-        // startButton.removeEventListener('click', startGame);
-        // changing the inner html to '...' while the computer plays
-        
-        //creating a for loop that loops through the possible moves and places one into the empty computersequence array
+        startButton.removeEventListener('click', playRound);
+        //creating a for loop that loops through the possible moves and places one into the computersequence 
         for (let i = 0; i < 1; i++){
             computerSequence.push(possibleMoves[Math.floor(Math.random() * 6)]);
             animateSequence();
@@ -48,7 +40,7 @@ console.log('connected');
                     startButton.innerHTML = 'Your Turn';
                 }
             }, 1000);
-        }//end of animate sequence
+        }
 
         //creating a function that animates each body part in a set amount of time
         function computerMove(bodypart){
@@ -57,45 +49,45 @@ console.log('connected');
             setTimeout(function(){
                 bodypart.style.background = '#fafafa';
             }, 500);
-        }//end of gameMove function
+        }
         console.log('computer plays', computerSequence);
-        getPlayerSequence();
-    } //new playround end
+        listenPlayerMove();
+    } // end of playround function
         
 
     //adding event listeners to all the body parts
-    function getPlayerSequence(){
+    function listenPlayerMove(){
         playerSequence = [];
         for (let i = 0; i < possibleMoves.length; i++){
-    	    possibleMoves[i].addEventListener('click',listenMove);
+    	    possibleMoves[i].addEventListener('click',getPlayerSequence);
         }
-    } // end of get player sequence function
-            
-    function listenMove(event){
-    // console.log(event.target.id);
-    // console.log(event);      
-        let playerMove = document.querySelector('#' + event.target.id);
+    } 
 
+    // locating the id of the clicked event and pushing it into the playerSequence array        
+    function getPlayerSequence(event){     
+        let playerMove = document.querySelector('#' + event.target.id);
         if (playerSequence.length !== round){
             playerSequence.push(playerMove);
         } 
         if (playerSequence.length === round){
             console.log('player plays', playerSequence);
-            document.querySelector('#bunny').removeEventListener('click', listenMove);
+            document.querySelector('#bunny').removeEventListener('click', getPlayerSequence);
             checkSequence();
         }
     } 
 
+
+    // checking to see if the playerSequence array is the same as the computer's
     function checkSequence(){
         console.log('checking sequence', round);
         var is_same = computerSequence.length == playerSequence.length && computerSequence.every(function(element, index) {
             return element === playerSequence[index]; 
         });
-         if(is_same && round === 5) {
+         if(is_same && round === 20) {
             startButton.innerHTML = 'You Win!';
             return false;
         }
-        if (is_same){
+        if(is_same){
             startButton.innerHTML = 'Great Job!';
             setTimeout(function(){
                 round++;
@@ -104,7 +96,6 @@ console.log('connected');
                 playRound();
             }, 1000);
         }
-         
         else {
             startButton.innerHTML = 'Uh Oh!';
             // restartButton.style.display = 'inline-block';
@@ -112,7 +103,7 @@ console.log('connected');
             return false;
         }
     }
-// } //end of startGame
+
 
 
 //creating a function that toggles the instructions text on and off on the click of the tab
@@ -124,11 +115,11 @@ function showInstructions(){
     else {
         instructions.style.display = 'none';
     }   
-} // end of showInstructions
+}
 
 
 window.onload = function() {
-//creating a function that toggles the instructions text on and off on the click of the ;
+    //setting an event listener to initiate the game on the click
     document.querySelector('#start').addEventListener('click', playRound);
     // setting an event listener to display the game instructions on the click
     document.querySelector('#instructions').addEventListener('click', showInstructions);
